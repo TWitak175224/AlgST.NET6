a7fff57fe4f90f9c7150a33285c3c3735e1db82e
TWitak175224 <161207572+TWitak175224@users.noreply.github.com>
2024-11-17 14:47:26 +0100

    update

--- AlgIST/AlgIST/BST.cs
+++ AlgIST/AlgIST/BST.cs
@@ -9,7 +9,50 @@ namespace AlgIST
 {
     internal class BST
     {
-        public NodeT root;
+        public NodeT root=null;
+
+        public void Add(int a)
+        {
+            NodeT dodawana = new NodeT(a);
+            if (this.root != null)
+            {
+                this.root = dodawana;
+            }
+            var rodzic = this.znajdzRodzica(dodawana);
+            dodawana.rodzic = rodzic;
+            if (rodzic.data < rodzic.data)
+            {
+                rodzic.lewe = dodawana;
+            }
+            else
+            {
+                rodzic.prawe = dodawana;
+            }
+        }
+        public NodeT znajdzRodzica(NodeT dziecko)
+        {
+            NodeT rodzic=this.root;
+            while (true)
+            {
+                if (dziecko.data < rodzic.data)
+                {
+                    if (rodzic.lewe == null)
+                    {
+                        return rodzic;
+                    }
+                    rodzic = rodzic.lewe;
+                }
+                else
+                {
+                    if (rodzic.prawe == null)
+                    {
+                        return rodzic;
+                    }
+                    rodzic = rodzic.prawe;
+                }
+            }
+         }
 
     }
+    
 }
