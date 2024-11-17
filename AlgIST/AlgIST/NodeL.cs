3a000aafb91ef50fe5a8e5a438d8ca104ca6f236
TWitak175224 <161207572+TWitak175224@users.noreply.github.com>
2024-11-17 14:35:03 +0100

    init

---
+++ AlgIST/AlgIST/NodeL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgIST
+{
+    internal class NodeL
+    {
+        public NodeL next, prev;
+        public int data;
+        public NodeL(int liczba)
+        {
+            this.data = liczba;
+        }
+    }
+}
---
+++ AlgIST/AlgIST/NodeT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgIST
+{
+    internal class NodeT
+    {
+       public NodeT rodzic,lewe,prawe;
+       public int data;
+
+        public NodeT(int liczba)
+        {
+            this.data = liczba;
+        }
+
+    }
+}
